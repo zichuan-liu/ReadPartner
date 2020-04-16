59e58677019d4ccf3c870882d8530e299ca38aca
lzc <775269512@qq.com>
2020-04-17 02:27:10 +0800

    壳子

---
+++ Tuikan/app/src/main/java/com/grace/zhihunews/ui/adapter/CategoriesAdapter.java
@@ -0,0 +1,52 @@
+package com.grace.zhihunews.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.grace.zhihunews.R;
+import com.grace.zhihunews.network.entity.video.Category;
+import com.zanlabs.widget.infiniteviewpager.InfinitePagerAdapter;
+
+import java.util.List;
+
+import io.vov.vitamio.utils.Log;
+
+/**
+ * Created by Administrator on 2016/9/17.
+ */
+public class CategoriesAdapter extends InfinitePagerAdapter {
+
+    private Context mContext;
+    private List<Category> mCategories;
+
+    public CategoriesAdapter(Context mContext, List<Category> mCategories) {
+        this.mContext = mContext;
+        this.mCategories = mCategories;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup container) {
+        GridView gridView;
+        if (convertView == null) {
+            gridView = (GridView) LayoutInflater.from(mContext).inflate(R.layout.item_viewpager, container, false);
+        } else {
+            gridView = (GridView) convertView;
+        }
+        gridView.setAdapter(new GridViewAdapter(mContext, mCategories, position));
+        return gridView;
+    }
+
+    @Override
+    public int getItemCount() {
+        int pageSize = 8;
+        int pageCount = (int) Math.ceil(mCategories.size() * 1.0 / pageSize);
+        return pageCount;
+    }
+
+    class ViewHolder {
+        GridView gridView;
+    }
+}
