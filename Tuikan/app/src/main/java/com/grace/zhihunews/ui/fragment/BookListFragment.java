dae4fc074dbe2f98988f7cd10e39dbe6db611d7e
lzc <775269512@qq.com>
2020-05-31 21:25:36 +0800

    fix2

--- Tuikan/app/src/main/java/com/grace/zhihunews/ui/fragment/BookListFragment.java
+++ Tuikan/app/src/main/java/com/grace/zhihunews/ui/fragment/BookListFragment.java
@@ -1,9 +1,18 @@
 package com.grace.zhihunews.ui.fragment;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -66,6 +75,7 @@ public class BookListFragment extends BaseFragment implements BookListContact.IB
     private List<Book> mBooks;
     private BooksAdapter booksAdapter;
     private Context mContext;
+    private String filePath;
 
     @Override
     protected int getLayoutResId() {
@@ -172,6 +182,12 @@ public class BookListFragment extends BaseFragment implements BookListContact.IB
                                         }else {
                                             newBook.setWriter(addDialog.getMessage());
                                         }
+                                        if (TextUtils.isEmpty(filePath)){
+                                            Toast.makeText(mContext, "未导入书籍", Toast.LENGTH_SHORT).show();
+                                            return;
+                                        }else {
+                                            newBook.setTxt_path(filePath);
+                                        }
                                         mBooks.add(newBook);
                                         booksAdapter.notifyDataSetChanged();
                                         addDialog.dismiss();
@@ -235,6 +251,151 @@ public class BookListFragment extends BaseFragment implements BookListContact.IB
     public void showLoadFailureMsg(String errorMsg) {
 
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            if ("file".equalsIgnoreCase(uri.getScheme())){//使用第三方应用打开
+                filePath = uri.getPath();
+                //                Toast.makeText(mContext,filePath+"11111",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {//4.4以后
+                filePath = getPath(mContext, uri);
+                //                Toast.makeText(mContext,filePath,Toast.LENGTH_SHORT).show();
+            } else {//4.4以下下系统调用方法
+                filePath = "";
+                Toast.makeText(mContext, "安卓版本太低", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
+    //    public String getRealPathFromURI(Uri contentUri) {
+    //        String res = null;
+    //        String[] proj = { MediaStore.Images.Media.DATA };
+    //        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+    //        if(null!=cursor&&cursor.moveToFirst()){
+    //            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+    //            res = cursor.getString(column_index);
+    //            cursor.close();
+    //        }
+    //        return res;
+    //    }
+
+    /**
+     * 专为Android4.4设计的从Uri获取文件绝对路径，以前的方法已不好使
+     */
+    @SuppressLint("NewApi")
+    public String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public String getDataColumn(Context context, Uri uri, String selection,
+                                String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
 
 }
